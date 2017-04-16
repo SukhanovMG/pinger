b58bbf174cef91cd2ee7ada7e313f15eb294ab09
SukhanovMG <maks140693@list.ru>
2017-04-16 21:37:49 +0300

    Доработал запускалку.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,44 @@
 #include "ping.h"
 
 #include <exception>
+#include <vector>
 #include <iostream>
 
 using namespace std;
 
+vector<string> addresses = { "ya.ru", "google.com", "8.8.8.8" };
+unsigned n_ping = 5;
+
+void ping_em_all()
+{
+	Pinger p(addresses[0]);
+
+	for (const auto& addr : addresses)
+	{
+		p.set_address(addr);
+		for (size_t i = 0; i < n_ping; i++)
+		{
+			try
+			{
+				p.ping();
+			}
+			catch (const runtime_error& re)
+			{
+				cout << re.what() << endl;
+			}
+		}
+	}
+}
+
 int main()
 {
 	try{
-		Pinger p("ya.ru");
-		p.ping();
-		p.set_address("google.com");
-		p.ping();
+		ping_em_all();
 	}
 	catch(exception& e)
 	{
 		cout << e.what() << endl;
+		return 1;
 	}
 	return 0;
 }
