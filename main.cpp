548479f53dbaea1b919030cc104689120770e4df
sukhanov <sukhanov@gollard.ru>
2017-04-24 17:08:20 +0300

    Заменил табы на пробелы, комментарии.

--- main.cpp
+++ main.cpp
@@ -11,36 +11,36 @@ unsigned n_ping = 5;
 
 void ping_em_all()
 {
-	Pinger p(addresses[0]);
+    Pinger p(addresses[0]);
 
-	for (const auto& addr : addresses)
-	{
-		cout << addr << endl;
-		p.set_address(addr);
-		for (size_t i = 0; i < n_ping; i++)
-		{
-			try
-			{
-				p.ping();
-			}
-			catch (const runtime_error& re)
-			{
-				cout << re.what() << endl;
-			}
-		}
-	}
+    for (const auto& addr : addresses)
+    {
+        cout << addr << endl;
+        p.set_address(addr);
+        for (size_t i = 0; i < n_ping; i++)
+        {
+            try
+            {
+                p.ping();
+            }
+            catch (const runtime_error& re)
+            {
+                cout << re.what() << endl;
+            }
+        }
+    }
 }
 
 int main()
 {
-	try{
-		ping_em_all();
-	}
-	catch(exception& e)
-	{
-		cout << e.what() << endl;
-		return 1;
-	}
-	return 0;
+    try{
+        ping_em_all();
+    }
+    catch(exception& e)
+    {
+        cout << e.what() << endl;
+        return 1;
+    }
+    return 0;
 }
 
--- ping.h
+++ ping.h
@@ -8,29 +8,29 @@
 class Socket
 {
 public:
-	Socket(int domain, int type, int protocol);
-	~Socket();
-	int get() const { return m_socket_fd; }
+    Socket(int domain, int type, int protocol);
+    ~Socket();
+    int get() const { return m_socket_fd; }
 private:
-	int m_socket_fd;
+    int m_socket_fd;
 };
 
 class Pinger
 {
 public:
-	enum defaults { kBufSize = 1500 };
-	Pinger(const std::string& address);
-	~Pinger();
+    enum defaults { kBufSize = 1500 };
+    Pinger(const std::string& address);
+    ~Pinger();
 
-	void set_address(const std::string& address) { m_address = address; }
+    void set_address(const std::string& address) { m_address = address; }
 
-	void ping();
+    void ping();
 private:
-	Socket m_sock;
-	std::string m_address;
+    Socket m_sock;
+    std::string m_address;
 
-	int m_transmitted;
-	int m_id;
+    int m_transmitted;
+    int m_id;
 };
 
 #endif
